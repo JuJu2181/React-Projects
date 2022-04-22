8cacf12ab83b4edc9504ea09a3aeca89d9d0f1f4
juju2181 <anishshilpakar8@gmail.com>
2022-04-22 23:49:34 +0545

    Completed First Project: Built a digital card

--- digital_business_card/src/components/Body.js
+++ digital_business_card/src/components/Body.js
@@ -1,15 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faFileText, faLink } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import resume  from "../assets/anish_shilpakar_resume.pdf"
 
 export default function Body() { 
     return (
         <div className="card-body">
             <h2 className="name"> Anish Shilpakar</h2>
             <span className="title">Software Engineer</span>
-            <p className="username"> @anish_shilpakar</p>
+            <a href="https://twitter.com/anish_shilpakar" className="username">@anish_shilpakar</a>
             <div className="btn-container">
-                <button className="btn email-btn">
-                    <i className="fa-solid fa-envelope"></i> Email</button>
-                <button className="btn linked-btn">LinkedIn</button>
+
+                    <a href="mailto:anishshilpakar8@gmail.com" className="btn email-btn">
+                    <FontAwesomeIcon icon={ faEnvelope}/>
+                    Email
+                    </a>
+                <a href={ resume } download="anish_shilpakar_resume" className="btn linked-btn">
+                    <FontAwesomeIcon icon={faFileText}/>
+                        Resume
+                    </a>
+            </div>
+            <div className="about">
+                <h2 className="content-title"> About</h2>
+                <p className="content-desc">I am an enthusiastic learner highly interested in full stack web development and Artificial intelligence. I am comfortable to work in teams and like working on projects.</p>
+            </div>
+            <div className="skill">
+                <h2 className="content-title">Skills</h2>
+                <p className="content-desc">Good programming knowledge of Python, JS, PHP, GIT, C, C++, HTML, CSS. Other Skills like time management, quick learning, communication skills, research skills.</p>
             </div>
         </div>
     );
--- digital_business_card/src/components/Foot.js
+++ digital_business_card/src/components/Foot.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare, faGithubSquare, faHashnode, faInstagramSquare, faLinkedin, faTwitter, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
 
 export default function Foot() { 
     return (
         <div className="card-footer">
-            
+            <a href="https://www.linkedin.com/in/anish-shilpakar13/">            <FontAwesomeIcon icon={faLinkedin} className="footer-icon"/></a>
+            <a href="https://github.com/JuJu2181">       <FontAwesomeIcon icon={faGithubSquare} className="footer-icon" /></a>
+            <a href="https://anish-shilpakar.com.np/">
+                <FontAwesomeIcon icon={ faHashnode} className="footer-icon"/>
+            </a>
+            <a href="https://twitter.com/anish_shilpakar"><FontAwesomeIcon icon={faTwitterSquare} className="footer-icon"/></a>
+            <a href="https://www.facebook.com/anish.shilpakar.JuJu13/">            <FontAwesomeIcon icon={faFacebookSquare} className="footer-icon" /></a>
         </div>
     );
 }
