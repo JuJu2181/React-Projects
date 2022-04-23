fccebbb4f77e61fc7d0aae1739afc719c5028f70
juju2181 <anishshilpakar8@gmail.com>
2022-04-23 22:45:20 +0545

    Added Hero Section in airbnb clone

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
-import Navbar from "./components/navbar"
+import Navbar from "./components/navbar";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
       <div>      
-        <Navbar/>
+      <Navbar />
+      <Hero />
       </div>
     )
 }
