97b24f9c032b6e6f74758f0361fb5f6ac881bc01
juju2181 <anishshilpakar8@gmail.com>
2022-04-27 22:07:31 +0545

    Learned basic of states

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,18 +1,27 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faImage} from "@fortawesome/free-solid-svg-icons";
+import memeData from "../memesData";
 
 export default function Meme() { 
+
+    const getMemeImage = () => { 
+        const memesArray = memeData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url 
+        console.log(url)
+    }
+
     return (
         <main>
-            <form className="meme-form">
+            <div className="meme-form">
                 <input type="text" className="form-input" placeholder="Top text"/>
                 <input type="text" className="form-input" 
                     placeholder="Bottom text"/>
-                <button className="form-btn">
+                <button className="form-btn" onClick={getMemeImage}>
                     Generate a new Meme 
                     <FontAwesomeIcon icon={ faImage} className="image-icon"/>
                 </button>
-            </form>
+            </div>
         </main>
     );
 }
