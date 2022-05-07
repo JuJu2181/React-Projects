e9bfefc3da902d137d9d162ef68c62191c03c0eb
juju2181 <anishshilpakar8@gmail.com>
2022-05-07 09:59:18 +0545

    feat: Added Dark Mode to info site and some responsiveness to digital card

--- react-info-site/src/App.js
+++ react-info-site/src/App.js
@@ -3,10 +3,17 @@ import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 function App() {
+
+  const [darkMode, setDarkMode] = React.useState(true);
+
+  const toggleDarkMode = () => { 
+    setDarkMode(prevMode => !prevMode)
+  }
+
   return (
     <div className="container">
-      <Navbar />
-      <Main />
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      <Main darkMode={ darkMode}/>
     </div>
   );
 }
--- react-info-site/src/components/Navbar.js
+++ react-info-site/src/components/Navbar.js
@@ -1,12 +1,23 @@
 import React from "react"
 import ReactLogo from "../images/logo.png"
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
-        <nav>
-            <img src={ ReactLogo } className="nav--icon" alt="React logo" />
+        <nav className={props.darkMode ? "dark": ""}>
+            <img src={ ReactLogo } className="nav--logo_icon" alt="React logo" />
             <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - Project 1</h4>
+            <div 
+                className="toggler" 
+            >
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
         </nav>
     )
 }
