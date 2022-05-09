d664187f03777a77da6779b07f972da2aaad200d
juju2181 <anishshilpakar8@gmail.com>
2022-05-09 22:24:14 +0545

    Completed adding main components and CSS for tenzy game

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,9 +1,37 @@
 import React from "react";
+import Dice from "./components/Dice";
+
 
 export default function App() { 
+    
+    // To generate list of 10 random numbers from 0 to 6
+    // algorithmic solution 
+    function allNewDiceA() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) { 
+            newDice.push(Math.ceil(Math.random()*6));
+        }
+        return newDice;
+    }
+    
+    const allNewDice = () => Array(10).fill().map(() => Math.ceil(Math.random() * 6));
+    
+    const [dice, setDice] = React.useState(allNewDice());
+
+    const diceElements = dice.map(dice => <Dice value={ dice}/>)
+
+    const handleRoll = () => { 
+        setDice(allNewDice());
+    }
+
     return <div className="main-wrapper">
         <main>
-            
+            <h1 className="game-title">Tenzies</h1>
+            <p className="game-desc">Roll until all dice are same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-wrapper">
+                { diceElements}
+            </div>
+            <button className="roll-btn" onClick={handleRoll}>Roll</button>
         </main>
     </div>
 }
