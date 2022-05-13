989db120951bc650fd3878ffb9a73eaa0c6312de
juju2181 <anishshilpakar8@gmail.com>
2022-05-13 13:58:17 +0545

    Finally completed popular component for recipe app

--- recipe-app/src/components/Popular.jsx
+++ recipe-app/src/components/Popular.jsx
@@ -6,6 +6,7 @@ import styled from "styled-components";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 // css for splide
 import "@splidejs/splide/dist/css/splide.min.css"; 
+import storedData from "../data";
 
 function Popular() {
 
@@ -19,19 +20,58 @@ function Popular() {
     
     // function to get popular recipes from api
     // asynchronous function is used here as request can take some time to accomplish 
-    const getPopular = async (recipeCount=9) => { 
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=${recipeCount}`);
-        const data = await api.json();
-        setPopular(data.recipes);
+    const getPopular = async (recipeCount = 10,fetchIfNotFound='localStorage') => {
+
+        /**
+         * Instead of fetching the data from every single time what I did was 
+         * to use some dummy data in a data.js file and then checking for   error and in case of error fetching data from data.js but even more efficient way of doing this is by using the local storage as a cache
+         */
+        if (fetchIfNotFound === 'localStorage') {
+            //* Firstly check if the item exists in localStorage if it doesn't exist we set an item instead 
+            const checkPopular = localStorage.getItem("popular");
+
+            // console.log(checkPopular);
+
+            if (checkPopular !== 'undefined') {
+                // If there is already an item called popular stored in localStorage then we will get the recipes from local Storage instead of sending fetch request
+                setPopular(JSON.parse(checkPopular));
+            } else {
+                // fetch data from api only when there is no item called popular in the localStorage
+                const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=${recipeCount}`);
+                const data = await api.json();
+                console.log(data)
+                localStorage.setItem("popular",JSON.stringify(data.recipes));
+                setPopular(data.recipes);
+            }
+        }
+        
+        else if (fetchIfNotFound === 'file') {
+            //** This is my own method for error handling and fetching data from a local file */
+            let data;
+            try {
+                const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=${recipeCount}`);
+                if (api.status !== 200) {
+                    throw new Error("Response code is not 200");
+                }
+                data = await api.json();
+                // console.log("Data from API");
+            } catch (e) {
+                console.log(`Error Found: ${e}\n so now using the default stored Data`);
+                data = storedData;
+            }
+            console.log(data);
+            setPopular(data.recipes);
+        }
     }
 
     // converting state data to jsx element
     const popularRecipes = popular.map(recipe => { 
         return (
-            <SplideSlide>
+            <SplideSlide key={ recipe.id}>
                 <Card>
                 <p>{recipe.title}</p>
-                <img src={recipe.image} alt={ recipe.title} />
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient />
                 </Card>
             </SplideSlide>
         ); 
@@ -93,5 +133,15 @@ const Card = styled.div`
         align-items: center;
     }
 `;
+
+// gradient style component 
+const Gradient = styled.div`
+    z-index: 3;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.5));
+`;
+
 
 export default Popular;
