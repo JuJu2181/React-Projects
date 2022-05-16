acddf67bd33a431c82fec6e642a68b123416ef58
juju2181 <anishshilpakar8@gmail.com>
2022-05-16 12:49:21 +0545

    feat: Connected frontend to backend using json-server

--- task-tracker/README.md
+++ task-tracker/README.md
@@ -1,3 +1,21 @@
 # Task Tracker
 A simple task tracker app similar to a todo app in which user can add task along with date and time and set reminder.  
 Also an about page is added.
+
+### Note: 
+For Production
+```
+npm run build
+```
+Install serve package globally
+```
+npm i -g serve
+```
+To run the local production server
+```
+serve -s build -p 8000
+```
+Created a mockup backend server using json-server 
+```
+npm i json-server
+```
--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from "./components/AddTask";
 // to import data from another source
-import tasksData from "./tasksdata";
+// import tasksData from "./tasksdata";
 // importing nanoid 
 import { nanoid } from 'nanoid';
 
@@ -13,27 +13,88 @@ export default function App() {
   const [showAddTask,setShowAddTask] = React.useState(false);
 
   // useState hook for tasks 
-  const [tasks, setTasks] = React.useState(tasksData)
+  const [tasks, setTasks] = React.useState([])
+
+  // useEffect to get data from api 
+  useEffect(() => { 
+    const getTasks = async () => { 
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks();
+  },[]);
+  
+  // function to fetch tasks data from api
+  const fetchTasks = async () => { 
+    const res = await fetch(process.env.REACT_APP_API_URL);
+    const data = await res.json();
+    return data;
+  }
+
+    // function to fetch a single task data from api
+    const fetchTask = async (id) => { 
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/${id}`);
+      const data = await res.json();
+      return data;
+    }
 
   // function to add Tasks 
-  const addTask = (task) => {
-    const id = nanoid();
-    const newTask = { id, ...task };
-    setTasks([newTask,...tasks]);
+  const addTask = async (task) => {
+    console.log(task);
+    const res = await fetch(`${process.env.REACT_APP_API_URL}`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task),
+    });
+
+    // data is the new task added in server
+    const data = await res.json();
+    console.log(data);
+
+    // Add this newly created task to tasks list 
+    setTasks([data,...tasks]);
+
+    // * We don't need to create a new id as the json-server will itself create an id for us
+    // const id = nanoid();
+    // const newTask = { id, ...task };
+    // setTasks([newTask,...tasks]);
   }
 
   // Delete task 
-  const deleteTask = (id) => { 
+  const deleteTask = async (id) => { 
+    // To delete tasks from server send a delete request
+    await fetch(`${process.env.REACT_APP_API_URL}/${id}`,
+          {
+            method: 'DELETE'
+          }
+          );
+
     setTasks(tasks.filter((task)=>task.id !== id));
   }
 
   // Toggle reminder of tasks 
-  const toggleReminder = (id) => { 
+  const toggleReminder = async (id) => { 
+    // To update the tasks reminder based on toggle in api server 
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+    // send a put request to update the task in server
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    });
+    const data = await res.json();
+
+    // to render updated tasks in frontend
     setTasks(tasks.map((prevTask) => { 
       return prevTask.id === id ?
         {
           ...prevTask,
-          reminder: !prevTask.reminder
+          reminder: data.reminder
         } :
         prevTask
     }));
