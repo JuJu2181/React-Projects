e26a05af0d1e68f83354053e562812434140038e
juju2181 <anishshilpakar8@gmail.com>
2022-05-20 23:11:52 +0545

    Added assets

---
+++ quiz-app/src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Button(props) {
+    return <div className="btn">{props.text}</div>;
+}
+
+export default Button;
--- quiz-app/src/pages/Home.jsx
+++ quiz-app/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Button from '../components/Button';
 
 function Home() {
   return (
-    <div>
-      Home
+    <div className='home'>
+      <h2>Quizzical</h2>
+      <p>A simple react quiz game</p>
+      <Button text="Start quiz"/>
     </div>
   )
 }
