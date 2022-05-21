b168d57f0dc3de1426b612019ebc9de93c9ab3fd
juju2181 <anishshilpakar8@gmail.com>
2022-05-21 14:19:20 +0545

    feat: Home Page Done

--- quiz-app/src/pages/Home.jsx
+++ quiz-app/src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Button from '../components/Button';
+import React from "react";
+import Button from "../components/Button";
+import TopBlob from "../components/TopBlob";
+import BottomBlob from "../components/BottomBlob";
 
 function Home() {
-  return (
-    <div className='home'>
-      <h2>Quizzical</h2>
-      <p>A simple react quiz game</p>
-      <Button text="Start quiz"/>
-    </div>
-  )
+    return (
+        <div className="home">
+            <TopBlob/>
+            <h2 className="home-title">Quizzical</h2>
+            <p className="home-desc">A simple react quiz game</p>
+            <Button text="Start Quiz" />
+            <BottomBlob />
+        </div>
+    );
 }
 
-export default Home
+export default Home;
