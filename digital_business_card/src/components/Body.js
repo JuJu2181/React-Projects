7706faa755f97dfab2e2e7aab46b66baee00adb3
juju2181 <anishshilpakar8@gmail.com>
2025-10-05 17:57:15 +0545

    Updated the digital business card with updated image and description

--- digital_business_card/src/components/Body.js
+++ digital_business_card/src/components/Body.js
@@ -8,7 +8,7 @@ export default function Body() {
     return (
         <div className="card-body">
             <h2 className="name"> Anish Shilpakar</h2>
-            <span className="title">Software Engineer</span>
+            <span className="title">Data Engineer</span>
             <a href="https://twitter.com/anish_shilpakar" className="username">@anish_shilpakar</a>
             <div className="btn-container">
 
@@ -23,11 +23,15 @@ export default function Body() {
             </div>
             <div className="about">
                 <h2 className="content-title"> About Me</h2>
-                <p className="content-desc">I am an enthusiastic learner highly interested in full stack web development and Artificial intelligence. I am comfortable to work in teams and like working on projects.</p>
+                <p className="content-desc">
+                I am a passionate Computer Engineer from Bhaktapur, Nepal, with over three years of experience in Data and AI. As an AWS Community Builder and Certified Solutions Architect, I design scalable cloud-based data solutions using AWS and Azure, leveraging Python, SQL, and Kubernetes for intelligent, data-driven innovation.
+                </p>
             </div>
             <div className="skill">
                 <h2 className="content-title">Skills</h2>
-                <p className="content-desc">Good programming knowledge of Python, JS, PHP, GIT, C, C++, HTML, CSS. Other Skills like time management, quick learning, communication skills, research skills.</p>
+                <p className="content-desc">
+                Skilled in Python (ML/DL), AWS, SQL, Docker, Kubernetes and Airflow. Experienced in React, Node, and PHP. Proficient in English, Nepali, Newari, and Hindi.
+                </p>
             </div>
         </div>
     );
